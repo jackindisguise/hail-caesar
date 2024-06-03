a0525b2de8d32fc80ee5195a3594ec97aa8dfb1a
Curtis Erickson <incognitus.me@gmail.com>
2024-06-03 13:00:17 -0500

    chore: starting to add database stuff

---
+++ src/database.ts
@@ -0,0 +1,54 @@
+import { readdir, readFile } from "fs/promises";
+import { join, dirname, extname } from "path";
+import { fileURLToPath } from "url";
+import { parse } from "toml";
+
+const rootPath = join(dirname(fileURLToPath(import.meta.url)), "..");
+const dataPath = join(rootPath, "data");
+
+// shared by races and classes
+import { Classification } from "./classification.js";
+
+/**
+ * Load races and shit.
+ */
+const racesPath = join(dataPath, "races");
+export const races: Classification[] = [];
+async function loadRaces() {
+	const files = await readdir(racesPath);
+	for (let file of files) {
+		const racePath = join(racesPath, file);
+		if (extname(racePath) !== ".toml") continue;
+		const data = await readFile(racePath, "utf8");
+		const json: any = parse(data);
+		const race = Classification.fromJSON(json);
+		if (race) races.push(race);
+	}
+}
+
+/**
+ * Load classes and shit.
+ */
+const classesPath = join(dataPath, "classes");
+export const classes: Classification[] = [];
+async function loadClasses() {
+	const files = await readdir(classesPath);
+	for (let file of files) {
+		const classPath = join(classesPath, file);
+		if (extname(classPath) !== ".toml") continue;
+		const data = await readFile(classPath, "utf8");
+		const json = parse(data);
+		const _class = Classification.fromJSON(json);
+		if (_class) classes.push(_class);
+	}
+}
+
+/**
+ * Front facing access to database loading.
+ * @param done Callback to run on successful database load.
+ */
+export async function load(done: Function) {
+	await loadRaces();
+	await loadClasses();
+	done();
+}
