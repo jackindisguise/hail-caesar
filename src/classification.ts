d34e2f6064c6045e9f80aab2421b5e2c70137529
Curtis Erickson <incognitus.me@gmail.com>
2024-06-03 12:59:06 -0500

    chore: implementing some validation for JSON and stuff

--- src/classification.ts
+++ src/classification.ts
@@ -1,1 +1,31 @@
-export class Classification {}
+interface ClassificationInterface {
+	name?: string;
+	description?: string;
+}
+
+export class Classification {
+	name?: string;
+	description?: string;
+
+	static validateInterface(data: unknown): ClassificationInterface | undefined {
+		if (!data) return;
+		if (typeof data !== "object") return;
+		const obj: Record<string, unknown> = data as Record<string, unknown>;
+		if (typeof obj.name !== "string") return;
+		if (typeof obj.description !== "string") return;
+		return obj as ClassificationInterface;
+	}
+
+	static fromJSON(data: unknown): Classification | undefined {
+		// validate JSON
+		const obj: ClassificationInterface | undefined =
+			Classification.validateInterface(data);
+		if (!obj) return;
+
+		// load classification
+		const c = new Classification();
+		if (obj.name) c.name = obj.name;
+		if (obj.description) c.description = obj.description;
+		return c;
+	}
+}
