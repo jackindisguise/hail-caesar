b61d674bc4333674f3e88868f9ec820ec8a7d892
Curtis Erickson <incognitus.me@gmail.com>
2024-06-03 12:59:56 -0500

    chore: not necessary but whatever

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-import { add } from "./build/init.js";
-console.log(add(1, 2));
+import { default as json2toml } from "json2toml";
+import { parse } from "toml";
+
+const json = {
+	name: "Josh",
+	password: "akls;dlajksqwopjreqw",
+	inventory: [
+		{
+			keywords: "excalibur sword legendary",
+			display: "The Legendary Sword, Excalibur",
+		},
+	],
+};
+
+const toml = json2toml(json, { newlineAfterSection: true });
+const parsed = parse(toml);
+console.log(json);
+console.log(toml);
+console.log(parsed);
