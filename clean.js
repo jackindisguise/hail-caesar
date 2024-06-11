ca5254f12359619154f99aa4daf651663ae7392f
Curtis Erickson <incognitus.me@gmail.com>
2024-06-11 14:08:13 -0500

    chore: clean script is a little more sane

--- clean.js
+++ clean.js
@@ -1,11 +1,28 @@
 import { rm } from "fs";
-import { join, dirname } from "path";
+import { join, dirname, relative } from "path";
 import { fileURLToPath } from "url";
 
 const dir = dirname(fileURLToPath(import.meta.url));
+const blocked = ["src", ".git", "node_modules", "data"];
 const args = process.argv.slice(2);
 for (let file of args) {
 	const joined = join(dir, file);
+	const rel = relative(dir, joined);
+	if (!rel) {
+		console.log("ignoring attempt to delete root");
+		continue;
+	}
+
+	if (rel.includes("..")) {
+		console.log("ignoring attempt to delete parent");
+		continue;
+	}
+
+	if (blocked.includes(rel)) {
+		console.log(`ignoring blocked path '${rel}'`);
+		continue;
+	}
+
 	rm(joined, { recursive: true, force: true }, (err) => {
 		console.log(`cleaned path '${file}'`);
 	});
