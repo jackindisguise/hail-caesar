500dfb9ff1aac40aec9128ea2204b2fa8d37a718
Curtis Erickson <incognitus.me@gmail.com>
2024-06-11 13:51:53 -0500

    chore: fully tested time module

--- src/time.spec.ts
+++ src/time.spec.ts
@@ -4,7 +4,7 @@ import * as time from "./time.js";
 const timeout = 2000;
 const ms = 500;
 describe("time.ts", () => {
-	xit("setAbsoluteInterval", (done) => {
+	it("setAbsoluteInterval", (done) => {
 		const start = Date.now();
 		let diff = 0;
 		let cycle = 0;
@@ -15,13 +15,13 @@ describe("time.ts", () => {
 			diff += now - target;
 		}, ms);
 		setTimeout(() => {
-			time.clearCustomInterval(intervalID);
+			time.clearCustomInterval(intervalID, 99999);
 			expect(diff / cycle).is.lessThanOrEqual(30); // diff is within 30ms
 			done();
 		}, timeout);
 	}).timeout(timeout * 1.1);
 
-	xit("setRelativeInterval", (done) => {
+	it("setRelativeInterval", (done) => {
 		const start = Date.now();
 		let diff = 0;
 		let cycle = 0;
@@ -32,7 +32,7 @@ describe("time.ts", () => {
 			diff += now - target;
 		}, ms);
 		setTimeout(() => {
-			time.clearCustomInterval(intervalID);
+			time.clearCustomInterval(intervalID, 99999);
 			expect(diff / cycle).is.lessThanOrEqual(30); // diff is within 30ms
 			done();
 		}, timeout);
