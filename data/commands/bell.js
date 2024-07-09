fe7a38ead77e5afb9759e6dd5b3fcfa32def3e56
Curtis Erickson <incognitus.me@gmail.com>
2024-07-09 11:46:46 -0500

    chore: forgot to delete these

--- data/commands/bell.js
+++
@@ -1,7 +0,0 @@
-export const keyword = "bell";
-export const syntax = "bell";
-export const description = "Ring a bell.";
-export const rule = /^(?:bell|bel|be|b)$/;
-export const script = (character, comment) => {
-	character.send("\x07");
-};
